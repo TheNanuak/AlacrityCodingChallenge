68cbf29f9bcf8468d7f6b81e97509dbd7e2d846e
David Petersen <petersendj@cardiff.ac.uk>
2024-03-27 14:35:15 +0000

    Added basic object init

--- Solution.py
+++ Solution.py
@@ -1,2 +1,24 @@
-#enter solution to problem 2 here \/
+#Problem 2 Solution
+        
+Company = []
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def creation(self):
+        Company.append(self)
+
+
+person = Person("Bob", 25)
+person.creation()
+person = Person("Alice", 20)
+person.creation()
+person = Person("Carol", 30)
+person.creation()
+person = Person("Dave", 35)
+person.creation()
+
+for p in Company:
+    print(p.name)
