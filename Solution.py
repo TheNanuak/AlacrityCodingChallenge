ff470e3a3ed19a49c49a28aa10ef5b3ca842b419
David Petersen <petersendj@cardiff.ac.uk>
2024-03-27 14:58:54 +0000

    Added calculations

--- Solution.py
+++ Solution.py
@@ -1,24 +1,60 @@
 #Problem 2 Solution
-        
+
+#init company list
 Company = []
 
-class Person:
+#define Human object
+class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+    #added self to company
     def creation(self):
         Company.append(self)
 
+#adding hard coded workers
+worker = Human("Bob", 25)
+worker.creation()
+worker = Human("Alice", 20)
+worker.creation()
+worker = Human("Carol", 30)
+worker.creation()
+worker = Human("Dave", 35)
+worker.creation()
 
-person = Person("Bob", 25)
-person.creation()
-person = Person("Alice", 20)
-person.creation()
-person = Person("Carol", 30)
-person.creation()
-person = Person("Dave", 35)
-person.creation()
+print(len(Company))
 
-for p in Company:
-    print(p.name)
+#calculations
+def Calculate(Company):
+    AvgAge = 0
+    Oldest = ""
+    Youngest = ""
+
+    for item in Company:
+        AvgAge = AvgAge + item.age
+
+    for item in Company:
+        if Oldest == "":
+            Oldest = item
+        else:
+            if item.age > Oldest.age:
+                Oldest = item
+
+    for item in Company:
+        if Youngest == "":
+            Youngest = item
+        else:
+            if item.age < Youngest.age:
+                Youngest = item
+
+    AvgAge = AvgAge / len(Company)
+    print("Youngest: " + str(Youngest.age))
+    print("Average: " + str(AvgAge))
+    print("Oldest: " + str(Oldest.age))
+    
+Calculate(Company)
+
+
+def interface():
+    print("Welcome to the company database.")
+    
